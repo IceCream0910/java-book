99f71b2ee45dc4838a2b8825c91ce8fb184705a5
Ice Cream <taein2370@naver.com>
2022-11-26 15:55:31 +0000

    UPDATE 초기 세팅

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,12 @@
 export default {
-  github: 'https://github.com/shuding/nextra',
-  docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  github: 'https://github.com/icecream0910/java-book',
+  docsRepositoryBase: 'https://github.com/icecream0910/java-book',
+  titleSuffix: ' – 잼민이도 이해하는 코딩',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">잼민이도 이해하는 코딩</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        JAVA
       </span>
     </>
   ),
@@ -15,16 +15,16 @@ export default {
       <meta name="msapplication-TileColor" content="#ffffff" />
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta httpEquiv="Content-Language" content="ko" />
+      <meta name="description" content="잼민이도 이해하는 코딩 : JAVA - 김노아, 윤태인" />
+      <meta name="og:description" content="잼민이도 이해하는 코딩 : JAVA - 김노아, 윤태인" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="twitter:image" content="https://i.imgur.com/GhrwelU.png" />
+      <meta name="twitter:site:domain" content="book.sungil.me" />
+      <meta name="twitter:url" content="https://book.sungil.me" />
+      <meta name="og:title" content="잼민이도 이해하는 코딩 : JAVA - 김노아, 윤태인" />
+      <meta name="og:image" content="https://i.imgur.com/GhrwelU.png" />
+      <meta name="apple-mobile-web-app-title" content="잼민이도 이해하는 코딩" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
@@ -61,7 +61,7 @@ export default {
   prevLinks: true,
   nextLinks: true,
   footer: true,
-  footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
-  unstable_faviconGlyph: '👋',
+  footerEditLink: 'Github에서 보기',
+  footerText: <>MIT {new Date().getFullYear()} © 김노아, 윤태인.</>,
+  unstable_faviconGlyph: '🤯',
 }
